28d74377284ee757ddc6842fe5ec4ba7bdc79714
Fabian Gazzia <if20b031@technikum-wien.at>
2021-11-12 00:30:31 +0100

    started arg. input from main

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,42 @@
-#include <iostream>
+/*
+* =======================================================================================================================================
+*
+*
+*                                                   Programm Description
+*
+*
+* =======================================================================================================================================
+*/
 
-int main()
+
+#include <iostream>
+#include <assert.h>
+#include <unistd.h> // for getopt();
+
+// temp weil ich das Makefile nicht verstehe
+#include "include/finder.h"
+
+using std::cout; using std::endl;
+using std::string;
+
+
+
+void PrintUsage(string ProgrammName)
 {
-    std::cout << "Hey, Jacob!" << std::endl;
+    cout << "Usage: " << ProgrammName << " [-R] [-i] searchpath filename1 [filename2] ...[filenameN]" << endl;
+}
+
+// int main(int argc, string argv[])
+int main(int argc, char* argv[])
+{
+    /*
+    std::cout << "start..." << std::endl;
+    int c;
+    std::string FileName, ProgramName;
+    unsigned short RArgumentCounter, iArgumentCounter;
+
+    ProgramName = argv[0];
+    std::cout << ProgramName << std::endl;
+    */
     return 0;
 }
