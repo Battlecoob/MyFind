c49cfa79b8ac5fa6355acf6709cdf6245c560aa7
jacob-sch <jacob.schmauss@gmail.com>
2021-11-22 19:25:45 +0100

    added program description

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,24 @@
 *
 * Program Description
 *
+*
+*This Programm is used to perform parallel file searching in a Linux filesystem.
+*
+*When starting the user has to provide arguments for the programm in the command line.
+*These arguments consist of two optional switches -R and -i which determine if the searches should be performed recursive and/or case-insensitive.
+*Other arguments are an absolute or relative path to start the search and at least one filename to search for.
+*If there is an error the user will get a short message on how to correctly use the programm
+*
+*If all arguments are correct the programm starts with creating an object of the MyFind class.
+*This object handles the flow of the programm and the parallelization
+*In the MyFork function the programm forks into different processes for each filename.
+*
+*Every process then creates an object of the Finder class which performs the actual file search with the given arguments.
+*If the search was succesfull the user gets the process id, the filename and the path to the found file as output.
+*
+*The parent process of the MyFind object then waits for all child processes to finish (WaitForChildren function)
+*If any errors happened the programm terminates all runnung zombie processes (KillTheUndead function)
+* 
 * =======================================================================================================================================
 */
 
